61dc9379aad9419a6b4591d18fed494d278dc466
FireFoje <vanechka.boyko891@gmail.com>
2023-08-17 16:02:20 +0300

    Изменено с учетом замечаний ментора: Hibernate.initialize(user.getRoles())-   LAZY сделать с использованием JOIN FETCH в UserRepository без initialize public void saveUser(User user) {     user.setRoles(Collections.singleton(roleService.getRoleByName("ROLE_USER"))); - не пойму, почему сохраняется любой пользователь всегда с ролью USER? при редактировании, при смене любого поля кроме пароля, пароль зашифруется и нельзя будет зайти.

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -41,9 +41,6 @@ public class User implements UserDetails {
     private String email;
     @ManyToMany(fetch = FetchType.LAZY)
     @Fetch(FetchMode.JOIN)
-//    @JoinTable(name = "users_roles",
-//            joinColumns = @JoinColumn(name = "user_id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
